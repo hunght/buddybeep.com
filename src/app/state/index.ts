d661ac22a86e2ca10dcefb7f6599bafb87e8def8
hunghoang <hth321@gmail.com>
2024-03-05 10:56:57 +0700

    fix local storage

--- src/app/state/index.ts
+++ src/app/state/index.ts
@@ -1,25 +1,115 @@
 import { atom } from 'jotai'
-import { atomWithImmer } from 'jotai-immer'
+import { withImmer } from 'jotai-immer'
 import { atomFamily, atomWithStorage } from 'jotai/utils'
 import { BotId, createBotInstance } from '~app/bots'
+import { BaichuanWebBot } from '~app/bots/baichuan'
+import { BingWebBot } from '~app/bots/bing'
+import { ChatGPTBot } from '~app/bots/chatgpt'
+import { ClaudeBot } from '~app/bots/claude'
+import { GeminiBot } from '~app/bots/gemini'
+import { GeminiProBot } from '~app/bots/gemini-api'
+import { GrokWebBot } from '~app/bots/grok'
+import { LMSYSBot } from '~app/bots/lmsys'
+import { PerplexityBot } from '~app/bots/perplexity'
+import { PiBot } from '~app/bots/pi'
+import { QianwenWebBot } from '~app/bots/qianwen'
+import { XunfeiBot } from '~app/bots/xunfei'
 import { FeatureId } from '~app/components/Premium/FeatureList'
+import { ChatState } from '~app/hooks/type'
+
 import { getDefaultThemeColor } from '~app/utils/color-scheme'
 import { Campaign } from '~services/server-api'
 import { ChatMessageModel } from '~types'
 import { uuid } from '~utils'
 
-type Param = { botId: BotId }
+type Param = { botId: BotId; agentId?: string }
+type OriginalChatState = {
+  botId: BotId
+  bot:
+    | ChatGPTBot
+    | BingWebBot
+    | GeminiBot
+    | ClaudeBot
+    | XunfeiBot
+    | LMSYSBot
+    | PiBot
+    | GeminiProBot
+    | QianwenWebBot
+    | BaichuanWebBot
+    | PerplexityBot
+    | GrokWebBot
+  messages: ChatMessageModel[]
+  generatingMessageId: string
+  abortController: AbortController | undefined
+  conversationId: string
+}
+const atomWithLocalStorage = (key: string, initialValue: OriginalChatState) => {
+  const getInitialValue = (): OriginalChatState => {
+    const item = localStorage.getItem(key)
+    if (item !== null) {
+      const serializeChatState: ChatState = JSON.parse(item)
+      console.log(`==== serializeChatState ===`)
+      console.log(serializeChatState)
+      console.log('==== end log ===')
+
+      initialValue.botId = serializeChatState.botId
+      initialValue.messages = serializeChatState.messages.map((m) => ({
+        id: m.id,
+        text: m.text,
+        image: m.image,
+        author: m.author,
+      }))
+      initialValue.bot.setcontextIds = serializeChatState.conversationContext.contextIds
+      initialValue.generatingMessageId = serializeChatState.generatingMessageId
+      initialValue.conversationId = serializeChatState.conversationId
+    }
+    return initialValue
+  }
+  const baseAtom = atom<OriginalChatState>(getInitialValue())
+  const derivedAtom = atom(
+    (get) => get(baseAtom),
+    (get, set, update) => {
+      const nextValue: OriginalChatState = typeof update === 'function' ? update(get(baseAtom)) : update
+      set(baseAtom, nextValue)
+
+      const messages = nextValue.messages.map((m) => ({
+        id: m.id,
+        text: m.text,
+        image: m.image,
+        error: m.error?.code,
+        author: m.author,
+      }))
+
+      const serializeChatState: ChatState = {
+        botId: nextValue.botId,
+        messages: messages,
+
+        generatingMessageId: nextValue.generatingMessageId,
+        conversationId: nextValue.conversationId,
+        conversationContext: { contextIds: nextValue.bot.contextIds },
+      }
+      console.log(`==== nextValue ===`)
+      console.log(nextValue)
+      console.log('==== end log ===')
+
+      localStorage.setItem(key, JSON.stringify(serializeChatState))
+    },
+  )
+  return derivedAtom
+}
 
 export const chatFamily = atomFamily(
   (param: Param) => {
-    return atomWithImmer({
+    const botSlug = param.botId + (param.agentId ? `-${param.agentId}` : '')
+    const initialValue = {
       botId: param.botId,
       bot: createBotInstance(param.botId),
       messages: [] as ChatMessageModel[],
       generatingMessageId: '',
       abortController: undefined as AbortController | undefined,
       conversationId: uuid(),
-    })
+    }
+    return withImmer(atomWithLocalStorage(botSlug, initialValue))
   },
   (a, b) => a.botId === b.botId,
 )
