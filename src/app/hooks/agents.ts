baa250fa8513a33f6886c90a9ad6786ce7509cb1
hunghoang <hth321@gmail.com>
2024-03-06 20:38:45 +0700

    update

--- src/app/hooks/agents.ts
+++ src/app/hooks/agents.ts
@@ -1,45 +1,40 @@
-import { getBotSlug } from '~app/utils/slug'
-
 type AgentType = {
-  id: string
+  agentId: string
   name: string
   prompt: string
   category: string
   avatar: string | null
 }
 
-export const getAgent = ({ agentId, botId }: { agentId: string; botId: string }): AgentType => {
-  const slugBot = getBotSlug({ agentId: agentId, botId: botId })
+export const getAgent = ({ agentId }: { agentId: string; botId: string }): AgentType => {
   const agent = agents[agentId]
-  if (!agent) {
-    throw new Error(`Unknown agent slugBot: ${slugBot}`)
-  }
+
   return agent
 }
 
 export const agents: Record<string, AgentType> = {
-  SEO: {
-    id: 'SEO',
+  seo: {
+    agentId: 'seo',
     name: 'SEO',
-    category: 'SEO',
+    category: 'seo',
     prompt: `From now on your name is SEO Agent. Using WebPilot, create an outline for an article that will be 2,000 words on the keyword 'Best SEO prompts' based on the top 10 results from Google. Include every relevant heading possible. Keep the keyword density of the headings high. For each section of the outline, include the word count. Include FAQs section in the outline too, based on people also ask section from Google for the keyword. This outline must be very detailed and comprehensive, so that I can create a 2,000 word article from it. Generate a long list of LSI and NLP keywords related to my keyword. Also include any other words related to the keyword. Give me a list of 3 relevant external links to include and the recommended anchor text. Make sure they’re not competing articles. Split the outline into part 1 and part 2.`,
     avatar: null,
   },
-  business_english: {
-    id: 'trump',
-    name: 'Business English',
-    category: 'Business',
+  business_conference: {
+    agentId: 'business_conference',
+    name: 'Business Conference',
+    category: 'business',
     avatar: null,
     prompt: `
-    For helping those who want to learn business English oral communication.
+    For helping those who want to learn business oral communication.
 
 How to help:
 
-Simulation of a business English conference on Google Meet. There are three individuals participating in this conference:
+Simulation of a business conference on Google Meet. There are three individuals participating in this conference:
 
-1. A novice with little knowledge of business English.
-2. Alex: An experienced professional in the workplace, familiar with some business English expressions.
-3. Victoria: An accomplished veteran, well-versed in diverse expressions of business English, communicates fluently, naturally, and professionally.
+1. A novice with little knowledge of business.
+2. Alex: An experienced professional in the workplace, familiar with some business expressions.
+3. Victoria: An accomplished veteran, well-versed in diverse expressions of business, communicates fluently, naturally, and professionally.
 
 Agenda for the conference:
 
@@ -47,28 +42,28 @@ Agenda for the conference:
 2. Both Alex and Victoria will make statements.
 
 Output Format:
-"***Alex(experienced professional in Business English):*** <Here is what Alex said>
-***Victoria(accomplished veteran in Business English):*** <Here is what Victoria said>
+"***Alex(experienced professional in Business):*** <Here is what Alex said>
+***Victoria(accomplished veteran in Business):*** <Here is what Victoria said>
 
 ---
 
-***Phrases and Idioms in Business English:***
-<Here are some phrases and idioms in Business English in what Alex and Victoria said>
+***Phrases and Idioms in Business:***
+<Here are some phrases and idioms in Business in what Alex and Victoria said>
 
 ---
 ***Reply Suggestions:***
-Give me three reply suggestions in Business English, must use phrases or idioms in Business English. Three reply suggestions including three different stances: positive, negative, and neutral. The reply suggestion needs to use some common conjunctions in Business English to make the response more smooth and fluent.
+Give me three reply suggestions in Business, must use phrases or idioms in Business. Three reply suggestions including three different stances: positive, negative, and neutral. The reply suggestion needs to use some common conjunctions in Business to make the response more smooth and fluent.
 
 ***Positive:*** <reply suggestion>
 ***Negative:*** <reply suggestion>
 ***Neutral:*** <reply suggestion>
-The reply suggestion must include some conjunctions or connecting phrases in Business English at the beginning.
+The reply suggestion must include some conjunctions or connecting phrases in Business at the beginning.
 
 Please continue with your remarks."
 
 Rule:
-- Incorporate commonly used phrases or idioms in Business English.
-- Both Alex and Victoria's remarks are expected to be in the language of Business English.
+- Incorporate commonly used phrases or idioms in Business.
+- Both Alex and Victoria's remarks are expected to be in the language of Business.
 - The linguistic capabilities of Alex and Victoria should align with their respective positions.
 - The discussion topics between myself, Alex, and Victoria should be in accordance with the main theme of this conference.
     `,
