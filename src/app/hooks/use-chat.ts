220a978065331a77a9a9240648f93c6c51fb2f8b
hunghoang <hth321@gmail.com>
2024-03-06 11:54:50 +0700

    update

--- src/app/hooks/use-chat.ts
+++ src/app/hooks/use-chat.ts
@@ -12,6 +12,10 @@ import { agents } from './agents'
 
 export function useChat(botId: BotId, agentId: string | null) {
   const chatAtom = useMemo(() => chatFamily({ botId, agentId }), [botId, agentId])
+  console.log(`==== chatAtom ===`)
+  console.log(chatAtom)
+  console.log('==== end log ===')
+
   const [chatState, setChatState] = useAtom(chatAtom)
 
   const updateMessage = useCallback(
@@ -123,6 +127,10 @@ export function useChat(botId: BotId, agentId: string | null) {
       setConversationMessages(botId, chatState.conversationId, chatState.messages)
     }
   }, [botId, chatState.conversationId, chatState.messages])
+  console.log(`==== agentId ===`)
+  console.log(agentId)
+  console.log(chatState.messages)
+  console.log('==== end log ===')
 
   const chat = useMemo(
     () => ({
@@ -133,9 +141,11 @@ export function useChat(botId: BotId, agentId: string | null) {
       resetConversation,
       generating: !!chatState.generatingMessageId,
       stopGenerating,
+      agentId,
     }),
     [
       botId,
+      agentId,
       chatState.bot,
       chatState.generatingMessageId,
       chatState.messages,
