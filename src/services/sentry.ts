d6892ace50272ca5586ef9a9a011d1951b5fe9c2
hunghoang <hth321@gmail.com>
2024-10-07 10:37:56 +0700

    fix again

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -1,13 +1,19 @@
-import * as Sentry from '@sentry/react'
+import { BrowserClient, defaultStackParser, getDefaultIntegrations, makeFetchTransport, Scope } from '@sentry/browser'
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/buddybeep\.com\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// filter integrations that use the global variable
+const integrations = getDefaultIntegrations({}).filter((defaultIntegration) => {
+  return !['BrowserApiErrors', 'Breadcrumbs', 'GlobalHandlers'].includes(defaultIntegration.name)
 })
+
+const client = new BrowserClient({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  transport: makeFetchTransport,
+  stackParser: defaultStackParser,
+  integrations: integrations,
+})
+
+const scope = new Scope()
+scope.setClient(client)
+
+client.init() // initializing has to be done after setting the client on the scope
+export { scope as Sentry }
